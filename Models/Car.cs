324b1f3d6d08396554ea5caced1165778fa348c7
shunjid <shunjid.rahman@gmail.com>
2023-03-11 23:25:31 +0600

    Add car model

---
+++ Models/Car.cs
@@ -0,0 +1,10 @@
+namespace Garage.Models;
+
+public class Car
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; } = null!;
+
+    public string? Color { get; set; } = null!;
+}
